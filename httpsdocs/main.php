0e7ded0b941eaaa48dc3f2f0e32b4cd7be437bf5
DaniJorCa <117183199+DaniJorCa@users.noreply.github.com>
2024-01-20 20:38:33 +0100

    Primera Version - SlideShow

---
+++ httpsdocs/main.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <link rel="stylesheet" href="../styles/header&footer.css">
+    <link rel="stylesheet" href="../styles/genericBody.css">
+    <link rel="stylesheet" href="../styles/bodyMain.css">
+    <link rel="stylesheet" href="../styles/slideshowArea.css">
+    <script type="text/javascript" src="../script/jsMain.js"></script>
+</head>
+<div class="capaBlur" id="capaBlur"></div>
+<?php include ('includes/header.html') ?>
+
+    <body>
+        
+        <div class="parallax">
+            <img class="logo-parallax" src="../logo/logoparallax.svg">
+        </div>
+        <div class="principal-objetivo">
+            <p>Tu satisfacción, nuestro principal objetivo</p>
+            <div class="cards">
+                <div class="card">
+                    <a class="icon-carrito carrito" href="carrito.php"><i class="fa-solid fa-cart-shopping fa-cart" style="color: #f3e9d2;"></i></a>
+                    <p>Introduce productos en su carrito y realiza la compra</p>
+                </div>
+                <div class="card">
+                    <i class="fa-solid fa-truck-fast" style="color: #f3e9d2;"></i>
+                    <p>Nosotros se lo haremos llegar</p>
+                </div>
+                <div class="card">
+                    <i class="fa-solid fa-gift" style="color: #f3e9d2;"></i>
+                    <div>
+                        <p>Le haremos entrega de aquello que tanto desea en</p>
+                        <p>24/48Hrs</p>
+                    </div>
+                </div>
+            </div>
+        </div>    
+        <?php include('includes/slideshowArea.html'); ?>
+
+        <div id="form-registro">
+            <h2>LOGIN / REGISTRO </h2>
+            <form method="POST" action="index.php">
+                <label for="email">Email</label>
+                <input name="email" class="email" type="email" placeholder="Email" autofocus required>
+                <label for="passwd">Contraseña</label>
+                <input name="passwd" class="passwd" type="text" placeholder="Contraseña" required>
+            </form>
+            <h3>¿Todavia no eres usuario?</h3>
+            <h3><a href="registrooooooo.php">Date de Alta</a></h3>
+        </div>
+    </body>
+    
+
+<?php include ('includes/footer.html') ?>
+</html>
---
+++ script/jsMain.js
@@ -0,0 +1,298 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let menusDesplegables = document.getElementsByClassName('item-show');
+
+Array.from(menusDesplegables).forEach(function(menuDesplegable) {
+    menuDesplegable.addEventListener('mouseenter', setClass);
+    menuDesplegable.addEventListener('mouseleave', unsetClass);
+});
+
+function setClass() {
+    let hijoflecha = this.querySelector('a i.fa-angle-down');
+    let menuDropdown = this.querySelector('.menu-dropdown');
+
+    if (hijoflecha) {
+        hijoflecha.classList.add('despliega');
+    }
+    if (menuDropdown) {
+        menuDropdown.classList.add('recubre');
+        menuDropdown.classList.remove('menu-dropdown');
+    }
+
+
+}
+
+function unsetClass() {
+    let hijoflecha = this.querySelector('a i.fa-angle-down');
+    let menuRecubre = this.querySelector('.recubre');
+
+    if (hijoflecha) {
+        hijoflecha.classList.remove('despliega');
+    }
+    if(menuRecubre){
+        menuRecubre.classList.add('menu-dropdown');
+        menuRecubre.classList.remove('recubre');
+    }
+}
+
+
+
+let btnsRegistrarUsuario = document.getElementsByClassName("login");
+let ventanaRegistro = document.getElementById('form-registro');
+let capaBlur = document.getElementsByClassName('capaBlur');
+let formularioRegistro = document.querySelector('#form-registro');
+console.log(formularioRegistro);
+
+
+for (const btnRegistrarUsuario of btnsRegistrarUsuario){
+    btnRegistrarUsuario.addEventListener('click', (e) => {
+        e.preventDefault();
+        ventanaRegistro.style.display = 'flex';
+        capaBlur[0].style.display = 'flex';
+        formularioRegistro.style.zIndex = '4';
+    });
+
+}
+
+capaBlur[0].addEventListener('click', (e) => {
+    let clickDentroCapaBlur = capaBlur[0].contains(e.target);
+
+    if(clickDentroCapaBlur){
+        console.log('Click fuera del formulario');
+        formularioRegistro.style.zIndex = '3';
+        capaBlur[0].style.display = "none";
+        ventanaRegistro.style.display = 'none';
+    }
+
+});
+
+
+/*let cardsArticulos = document.getElementsByClassName('articulo');
+let botonDesplazamientoIzq = document.getElementById('button-slideshow-left');
+let botonDesplazamientoDer = document.getElementById('button-slideshow-right');
+let inicioTiempo = new Date().getTime();
+let tiempoTranscurrido = 0;
+let intervaloPosicion;
+
+function desplazarIzquierda() {
+    for(articulo of cardsArticulos){
+        articulo.style.transition = 'transform 3s linear';
+        articulo.style.transform = 'translateX(-100%)'; 
+    }
+        tiempoTranscurrido = 0;
+        intervaloPosicion = setInterval(function() {
+        let tiempoTranscurrido = new Date().getTime() - inicioTiempo;
+        console.log("Tiempo transcurrido: " + tiempoTranscurrido + " milisegundos");
+    }, 30);
+  }
+
+  function desplazarDerecha() {
+    for(articulo of cardsArticulos){
+        articulo.style.transition = 'transform 3s linear';
+        articulo.style.transform = 'translateX(100%)';
+    }
+        tiempoTranscurrido = 0;
+        intervaloPosicion = setInterval(function() {
+        let tiempoTranscurrido = new Date().getTime() - inicioTiempo;
+        console.log("Tiempo transcurrido: " + tiempoTranscurrido + " milisegundos");
+    }, 30);
+  }
+
+  function pausarAnimacion() {
+    clearInterval(intervaloPosicion);
+    for (articulo of cardsArticulos) {
+        articulo.style.transition = 'none';
+        articulo.style.transform = `translateX(${calcularPosicion()}%)`;
+    }
+}
+
+
+function calcularPosicion() {
+    let porcentajeDesplazamiento = (tiempoTranscurrido * 100) / 3000;
+    return porcentajeDesplazamiento;
+}
+
+  botonDesplazamientoIzq.addEventListener('mouseenter', desplazarIzquierda);
+  botonDesplazamientoIzq.addEventListener('mouseleave', pausarAnimacion);
+
+  botonDesplazamientoDer.addEventListener('mouseenter', desplazarDerecha);
+  botonDesplazamientoDer.addEventListener('mouseleave', pausarAnimacion);*/
+
+
+  
+
+/* Funciones Y Calculo de distancias de SlideShow*/  
+
+const articulos = document.getElementsByClassName('articulo');
+let divArticulos = document.getElementsByClassName('articulos');
+let anchoTotalArticulo = calcularAnchoTotalArticulo('articulos' , '.articulo');
+let anchoTotalDivArticulos = calcularDimensionDivArticulos(articulos, anchoTotalArticulo);
+let anchoVisibleDivArticulos = divArticulos[0].clientWidth;
+let desplazamientoTotalLateralMaximo = desplazamientoLateralMaximo(articulos, anchoTotalArticulo, anchoVisibleDivArticulos);
+
+/*for (articulo of articulos) {
+    articulo.style.transform = `translateX(-${desplazamientoTotalLateralMaximo}px)`;
+}*/
+
+
+  function calcularDimensionDivArticulos(articulos, anchoTotalArticulo){
+    anchoTotal = ((articulos.length * anchoTotalArticulo) + ((articulos.length - 2) * 20));
+    return anchoTotal;
+  }
+
+  function calcularAnchoTotalArticulo(claseDivParent, claseDivChild){
+    let estilosArticulo = extraerEstilosDivChild(`${claseDivParent}`, `${claseDivChild}`);
+    let anchoArticulo = parseFloat(estilosArticulo.width) + parseFloat(estilosArticulo.marginLeft) + parseFloat(estilosArticulo.marginRight);
+    return anchoArticulo;
+  }
+
+  function extraerEstilosDivChild (claseDivParent, claseDivChild){
+    let div = document.getElementsByClassName(`${claseDivParent}`);
+    let estilos = getComputedStyle(div[0].querySelector(`${claseDivChild}`));
+    return estilos;
+  }
+
+  function desplazamientoLateralMaximo(articulos, anchoTotalArticulo, anchoVisibleDivArticulos){
+    desplazamientoMaximo = (((articulos.length * anchoTotalArticulo) + ((articulos.length -2) * 20)) - anchoVisibleDivArticulos)/2;
+    return desplazamientoMaximo;
+}
+
+    desplazamientoLateralMaximo(articulos, anchoTotalArticulo, anchoVisibleDivArticulos);
+    console.log('Parte visible Div articulos ' + anchoVisibleDivArticulos);
+    console.log('Ancho total Div Articulos ' + anchoTotalDivArticulos);
+    console.log('Ancho total articulo ' + anchoTotalArticulo);
+    console.log ('Desplazamiento Total lateral Maximo ' + desplazamientoTotalLateralMaximo);
+
+
+  function actualizarDimensiones(){
+    let anchoTotalArticulo = calcularAnchoTotalArticulo('articulos' , '.articulo');
+    let anchoTotalDivArticulos = calcularDimensionDivArticulos(articulos, anchoTotalArticulo);
+    let anchoVisibleDivArticulos = divArticulos[0].clientWidth;
+    let desplazamientoTotalLateralMaximo = desplazamientoLateralMaximo(articulos, anchoTotalArticulo, anchoVisibleDivArticulos);
+    console.log('Parte visible Div articulos ' + anchoVisibleDivArticulos);
+    console.log('Ancho total Div Articulos ' + anchoTotalDivArticulos);
+    console.log('Ancho total articulo ' + anchoTotalArticulo);
+    console.log ('Desplazamiento Total lateral Maximo ' + desplazamientoTotalLateralMaximo);
+  }
+  
+  window.addEventListener('resize', function() {
+    actualizarDimensiones();
+  });
+
+
+/* Parte Funcional SlideShow */
+
+let estilos = extraeEstilos('articulos', '.articulo');
+
+function calculoTiempoDesplazamiento(espacioPorRecorrer){
+    let time = (espacioPorRecorrer * 4) / 900;
+    if(time > 2){
+        time == 2;
+    }
+    return time;
+}
+
+function extraeEstilos(claseDivParent, claseDivChild){
+    let estilosArticulo = extraerEstilosDivChild(`${claseDivParent}`, `${claseDivChild}`);
+    return estilosArticulo;
+}
+
+function extraeTranslateX(estilos){
+    let estiloTransform = estilos.getPropertyValue('transform');
+    let valoresTransform = new DOMMatrix(estiloTransform);
+    let translateX = valoresTransform.m41;
+    return translateX;
+}
+
+
+function espacioPorRecorrer(articulos , articulo, desplazamientoTotalLateralMaximo){
+    let estilos = extraeEstilos(`${articulos}`, `${articulo}`);
+    let estiloTransform = estilos.getPropertyValue('transform');
+    let valoresTransform = new DOMMatrix(estiloTransform);
+    let translateX = valoresTransform.m41; // Valor de translateX
+    let espacioARecorrer = desplazamientoTotalLateralMaximo - translateX;
+    return espacioARecorrer; 
+}
+
+  let cardsArticulos = document.getElementsByClassName('articulo');
+  let botonDesplazamientoIzq = document.getElementById('button-slideshow-left');
+  let botonDesplazamientoDer = document.getElementById('button-slideshow-right');
+
+
+
+  function desplazarIzquierda() {
+    let espacioARecorrer = espacioPorRecorrer('articulos','.articulo', desplazamientoTotalLateralMaximo);
+    if(espacioARecorrer){
+        let tiempoNecesarioParaRecorrer = calculoTiempoDesplazamiento(espacioARecorrer);
+        for (articulo of cardsArticulos) {
+            console.log(tiempoNecesarioParaRecorrer + ' tiempo izquierda');
+            articulo.style.transitionProperty = 'transform';
+            articulo.style.transitionDuration = `${tiempoNecesarioParaRecorrer}s`;
+            articulo.style.transitionTimingFunction = 'linear';
+            articulo.style.transform = `translateX(-${desplazamientoTotalLateralMaximo}px)`;
+        } 
+    }else{
+        for (articulo of cardsArticulos) {
+            articulo.style.transitionProperty = 'transform';
+            articulo.style.transitionDuration = `3s`;
+            articulo.style.transitionTimingFunction = 'linear';
+            articulo.style.transform = `translateX(-${desplazamientoTotalLateralMaximo}px)`;
+        }  
+    }
+    
+      posicionExacta = setInterval(function () {
+        posX = extraeTranslateX(estilos);
+        }, 1);
+  }
+  
+  function desplazarDerecha() {
+    let espacioARecorrer = espacioPorRecorrer('articulos','.articulo', desplazamientoTotalLateralMaximo);
+    if(espacioARecorrer){
+        let tiempoNecesarioParaRecorrer = calculoTiempoDesplazamiento(espacioARecorrer);
+        for (articulo of cardsArticulos) {
+            console.log(tiempoNecesarioParaRecorrer + ' tiempo derecha');
+            articulo.style.transitionProperty = 'transform';
+            articulo.style.transitionDuration = `${tiempoNecesarioParaRecorrer}s`;
+            articulo.style.transitionTimingFunction = 'linear';
+            articulo.style.transform = `translateX(${desplazamientoTotalLateralMaximo}px)`;
+        } 
+    }else{
+        for (articulo of cardsArticulos) {
+            articulo.style.transitionProperty = 'transform';
+            articulo.style.transitionDuration = `3s`;
+            articulo.style.transitionTimingFunction = 'linear';
+            articulo.style.transform = `translateX(${desplazamientoTotalLateralMaximo}px)`;
+        }  
+    }
+      
+      posicionExacta = setInterval(function () {
+        posX = extraeTranslateX(estilos);
+      }, 1);
+  }
+
+
+  function pausarAnimacionDer() {
+      clearInterval(posicionExacta);
+      for (articulo of cardsArticulos) {
+          articulo.style.transition = 'none';
+          articulo.style.transform = `translateX(${posX}px)`;
+      }
+  }
+
+  function pausarAnimacionIzq() {
+    clearInterval(posicionExacta);
+    for (articulo of cardsArticulos) {
+        articulo.style.transition = 'none';
+        articulo.style.transform = `translateX(${posX}px)`;
+    }
+}
+  botonDesplazamientoIzq.addEventListener('mouseenter', desplazarIzquierda);
+  botonDesplazamientoIzq.addEventListener('mouseleave', pausarAnimacionIzq);
+  
+  botonDesplazamientoDer.addEventListener('mouseenter', desplazarDerecha);
+  botonDesplazamientoDer.addEventListener('mouseleave', pausarAnimacionDer);
+
+
+}); 
+
